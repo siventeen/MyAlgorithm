29202e6dbfa9f308de4b1af6d3cecc21e0df06c4
wangshiqi <wangshiqi@bw30.com>
2025-04-19 23:53:06 +0800

    leetcode698

--- src/LeetCode698.java
+++ src/LeetCode698.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
+// 以数字的视角选择桶
 public class LeetCode698 {
     public boolean canPartitionKSubsets(int[] nums, int k) {
         // 排除一些基本情况
@@ -52,6 +54,71 @@ public class LeetCode698 {
             bucket[i] -= nums[index];
         }
         // nums[index] 装入哪个桶都不行
+        return false;
+    }
+}
+
+// 以桶的视角选择数字
+class Solution698_2 {
+    HashMap<Integer, Boolean> memo = new HashMap<>();
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        // 排除一些基本情况
+        if (k > nums.length) return false;
+        int sum = 0;
+        for (int v : nums) sum += v;
+        if (sum % k != 0) return false;
+
+        // 使用位图技巧
+        int used = 0;
+        int target = sum / k;
+        // k 号桶初始什么都没装，从 nums[0] 开始做选择
+        return backtrack(k, 0, nums, 0, used, target);
+    }
+    boolean backtrack(int k, int bucket,
+                      int[] nums, int start, int used, int target) {
+        // base case
+        if (k == 0) {
+            // 所有桶都被装满了，而且 nums 一定全部用完了
+            return true;
+        }
+        if (bucket == target) {
+            // 装满了当前桶，递归穷举下一个桶的选择
+            // 让下一个桶从 nums[0] 开始选数字
+            boolean res = backtrack(k - 1, 0, nums, 0, used, target);
+            // 缓存结果
+            memo.put(used, res);
+            return res;
+        }
+
+        if (memo.containsKey(used)) {
+            // 避免冗余计算
+            return memo.get(used);
+        }
+
+        for (int i = start; i < nums.length; i++) {
+            // 剪枝
+            // 判断第 i 位是否是 1
+            if (((used >> i) & 1) == 1) {
+                // nums[i] 已经被装入别的桶中
+                continue;
+            }
+            if (nums[i] + bucket > target) {
+                continue;
+            }
+            // 做选择
+            // 将第 i 位置为 1
+            used |= 1 << i;
+            bucket += nums[i];
+            // 递归穷举下一个数字是否装入当前桶
+            if (backtrack(k, bucket, nums, i + 1, used, target)) {
+                return true;
+            }
+            // 撤销选择
+            // 使用异或运算将第 i 位恢复 0
+            used ^= 1 << i;
+            bucket -= nums[i];
+        }
+
         return false;
     }
 }
