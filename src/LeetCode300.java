5b34e25bbc37e14a542ac180d9d68cf35503db88
wangshiqi <wangshiqi@bw30.com>
2025-05-23 17:21:07 +0800

    LeetCode300

---
+++ src/LeetCode300.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class LeetCode300 {
+    public int lengthOfLIS(int[] nums) {
+        // dp[i] 表示以nums[i]结尾的最长递增子序列的长度
+        int[] dp = new int[nums.length];
+        // base case：dp 数组全都初始化为 1
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int res = 0;
+        for (int j : dp) {
+            res = Math.max(res, j);
+        }
+        return res;
+    }
+}
