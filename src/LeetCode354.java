0cee3d87d88842a7d7392a1a744fec5a68aa4965
wangshiqi <wangshiqi@bw30.com>
2025-05-26 14:58:39 +0800

    LeetCode354

--- src/LeetCode354.java
+++ src/LeetCode354.java
@@ -1,6 +1,7 @@
 import com.sun.xml.internal.bind.v2.model.core.EnumLeafInfo;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class LeetCode354 {
     public int maxEnvelopes(int[][] envelopes) {
@@ -35,5 +36,50 @@ public class LeetCode354 {
             res = Math.max(res, j);
         }
         return res;
+    }
+
+    public static class solution2 {
+        public int maxEnvelopes(int[][] envelopes) {
+            int n = envelopes.length;
+            // 按宽度升序排列，如果宽度一样，则按高度降序排列
+            Arrays.sort(envelopes, new Comparator<int[]>()
+            {
+                public int compare(int[] a, int[] b) {
+                    return a[0] == b[0] ?
+                            b[1] - a[1] : a[0] - b[0];
+                }
+            });
+            // 对高度数组寻找 LIS
+            int[] height = new int[n];
+            for (int i = 0; i < n; i++)
+                height[i] = envelopes[i][1];
+
+            return lengthOfLIS(height);
+        }
+
+
+        /* 返回 nums 中 LIS 的长度 */
+        public int lengthOfLIS(int[] nums) {
+            int piles = 0, n = nums.length;
+            int[] top = new int[n];
+            for (int i = 0; i < n; i++) {
+                // 要处理的扑克牌
+                int poker = nums[i];
+                int left = 0, right = piles;
+                // 二分查找插入位置
+                while (left < right) {
+                    int mid = (left + right) / 2;
+                    if (top[mid] >= poker)
+                        right = mid;
+                    else
+                        left = mid + 1;
+                }
+                if (left == piles) piles++;
+                // 把这张牌放到牌堆顶
+                top[left] = poker;
+            }
+            // 牌堆数就是 LIS 长度
+            return piles;
+        }
     }
 }
