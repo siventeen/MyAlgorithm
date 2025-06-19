5c9080c5ef7f3b0cabc2d33edcc63c77d544811c
wangshiqi <wangshiqi@bw30.com>
2025-06-19 18:34:16 +0800

    LeetCode134

---
+++ src/LeetCode134.java
@@ -0,0 +1,23 @@
+public class LeetCode134 {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int n = gas.length;
+        // 相当于图像中的坐标点和最低点
+        int sum = 0, minSum = 0;
+        int start = 0;
+        for (int i = 0; i < n; i++) {
+            sum += gas[i] - cost[i];
+            if (sum < minSum) {
+                // 经过第 i 个站点后，使 sum 到达新低
+                // 所以站点 i + 1 就是最低点（起点）
+                start = i + 1;
+                minSum = sum;
+            }
+        }
+        if (sum < 0) {
+            // 总油量小于总的消耗，无解
+            return -1;
+        }
+        // 环形数组特性
+        return start == n ? 0 : start;
+    }
+}
