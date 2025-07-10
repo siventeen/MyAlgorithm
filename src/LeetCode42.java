237f7f90627ccf98c3a79a0a90d83f1c6eec3fb4
wangshiqi <wangshiqi@bw30.com>
2025-07-10 17:08:46 +0800

    LeetCode42

---
+++ src/LeetCode42.java
@@ -0,0 +1,37 @@
+public class LeetCode42 {
+    public static int trap(int[] height) {
+        int result = 0;
+        int n = height.length;
+        int left = 0;
+        int right = n - 1;
+        while ( left < n - 1 && (height[left] == 0 || height[left] <= height[left + 1])) {
+            left++;
+        }
+        while (right > left && (height[right] == 0 || height[right] <= height[right - 1])) {
+            right--;
+        }
+
+        while (left < right) {
+            int curHeight = Math.min(height[left], height[right]);
+            int curLeft = left;
+            int curRight = right;
+            if (height[left] < height[right]) {
+                while (left < right && height[curLeft] >= height[left]) {
+                    result += (curHeight - height[left]);
+                    left++;
+                }
+            } else {
+                while (left < right && height[curRight] >= height[right]) {
+                    result += (curHeight - height[right]);
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] h = new int[]{0};
+        int res = trap(h);
+    }
+}
